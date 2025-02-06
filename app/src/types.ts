0b155243463423753efb7984c20fa8c705b7795b
thomaspantioras <t.pantioras@gmail.com>
2025-02-06 19:52:09 +0200

    custom types

--- app/src/types.ts
+++ app/src/types.ts
@@ -3,11 +3,18 @@ export type Restaurant = {
   name: string
   address: string
   website: string
-  status: string
+  status: Status
+  diet?: Diet
 }
 
 export type Dish = {
   id: string
   name: string
-  status: string
+  status: Status
+  diet?: Diet
 }
+
+
+type Status = 'Want to Try' | 'Recommended' | 'Do Not Recommend'
+
+type Diet = 'Vegetarian' | 'Pescetarian' | 'Keto' | ''
